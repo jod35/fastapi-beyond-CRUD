c615fa1f7037bbe231fb5776b7c221391e17b938
jod35 <jodestrevin@gmail.com>
2024-06-20 10:47:04 +0300

    add jwt dependencies

---
+++ src/auth/dependencies.py
@@ -0,0 +1,55 @@
+from fastapi import Request, status
+from fastapi.security import HTTPBearer
+from fastapi.security.http import HTTPAuthorizationCredentials
+from .utils import decode_token
+from fastapi.exceptions import HTTPException
+
+
+class TokenBearer(HTTPBearer):
+
+    def __init__(self, auto_error=True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+        creds = await super().__call__(request)
+
+        token = creds.credentials
+
+        token_data = decode_token(token)
+
+        if not self.token_valid:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or expired token"
+            )
+        
+        self.verify_token_data(token_data)
+
+        return token_data
+
+    def token_valid(self, token: str) -> bool:
+
+        token_data = decode_token(token)
+
+        return True if token_data is not None else False
+    
+    def verify_token_data(self,token_data):
+        raise NotImplementedError("Please Override this method in child classes")
+
+
+class AccessTokenBearer(TokenBearer):
+
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data["refresh"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an access token",
+            )
+
+
+class RefreshTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and not token_data["refresh"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide a refresh token",
+            )
--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter, Depends, status
-from fastapi.exceptions import HTTPException
-from sqlmodel.ext.asyncio.session import AsyncSession
-from src.db.main import get_session
 from .schemas import UserCreateModel, UserModel, UserLoginModel
-from .utils import create_access_token, check_password
 from .service import UserService
-from .auth_handler import security
-from typing import List
-from datetime import timedelta
+from src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from fastapi.exceptions import HTTPException
+from .utils import create_access_token, decode_token, verify_password
+from fastapi.responses import JSONResponse
+from datetime import timedelta, datetime
+from .dependencies import RefreshTokenBearer
 
 auth_router = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRY = 2
+
+
+# Bearer Token
 
 
 @auth_router.post(
@@ -34,52 +39,52 @@ async def create_user_Account(
     return new_user
 
 
-@auth_router.get(
-    "/users", status_code=status.HTTP_200_OK, response_model=List[UserModel]
-)
-async def get_all_users(session: AsyncSession = Depends(get_session)):
-    users = await user_service.get_all_users(session)
-
-    return users
-
-
-@auth_router.post("/login", status_code=status.HTTP_200_OK)
-async def login_user(
-    user_data: UserLoginModel, session: AsyncSession = Depends(get_session)
+@auth_router.post("/login")
+async def login_users(
+    login_data: UserLoginModel, session: AsyncSession = Depends(get_session)
 ):
-    email = user_data.email
-    password = user_data.password
+    email = login_data.email
+    password = login_data.password
 
     user = await user_service.get_user_by_email(email, session)
 
-    if user is not None and check_password(password, user.password_hash):
-        access_token = create_access_token({"user_id": str(user.uid)})
-        refresh_token = create_access_token(
-            {
-                "user_id": str(user.uid),
-                "refresh":True
-            },
-            expires=timedelta(days=2),
-        )
+    if user is not None:
+        password_valid = verify_password(password, user.password_hash)
 
-        return {
-            "message": "Login Successful",
-            "access_token": access_token,
-            "refresh_token": refresh_token,
-            "user": user,
-        }
+        if password_valid:
+            access_token = create_access_token(
+                user_data={"email": user.email, "user_uid": str(user.uid)}
+            )
 
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Email Or Password"
-        )
+            refresh_token = create_access_token(
+                user_data={"email": user.email, "user_uid": str(user.uid)},
+                refresh=True,
+                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
+            )
+
+            return JSONResponse(
+                content={
+                    "message": "Login successful",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user": {"email": user.email, "uid": str(user.uid)},
+                }
+            )
+
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Email Or Password"
+    )
 
 
-@auth_router.post("/refresh_token", status_code=status.HTTP_200_OK)
-async def refresh_token():
-    pass
+@auth_router.get("/refresh_token")
+async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details["exp"]
 
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data=token_details["user"])
 
-@auth_router.post("/logout", status_code=status.HTTP_200_OK)
-async def logout_users():
-    pass
+        return JSONResponse(content={"access_token": new_access_token})
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Or expired token"
+    )
