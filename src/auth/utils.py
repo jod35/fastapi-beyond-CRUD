dfc7d03d15bd1c757c4f8a89b2f169d169cf01fc
jod35 <jodestrevin@gmail.com>
2024-06-20 10:52:35 +0300

    add jwt dependencies

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,61 +1,55 @@
-from datetime import datetime, timedelta
-
-import jwt
 from passlib.context import CryptContext
-
+from datetime import timedelta, datetime
 from src.config import Config
+import jwt
+import uuid
+import logging
 
-PASSWORD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = Config.SECRET_KEY
-ALGORITHM = Config.ALGORITHM
-
-
-def check_password(plain_password: str, hashed_password: str) -> bool:
-    """Check and verify your password against a hash"""
-    return PASSWORD_CONTEXT.verify(plain_password, hashed_password)
+passwd_context = CryptContext(schemes=["bcrypt"])
 
 
-def generate_passwd_hash(plain_password: str) -> str:
-    """Hash a password to be stored in the database"""
-    return PASSWORD_CONTEXT.hash(plain_password)
+ACCESS_TOKEN_EXPIRY = 3600
 
 
-def create_access_token(data: dict, expires: timedelta | None = None):
-    """Create new access token for authorization"""
-    data = data.copy()
+def generate_passwd_hash(password: str) -> str:
+    hash = passwd_context.hash(password)
 
-    now = datetime.now()
+    return hash
 
-    if not expires:
-        expires = timedelta(minutes=30)
 
-    data.update({"exp": now + expires})
+def verify_password(password: str, hash: str) -> bool:
+    return passwd_context.verify(password, hash)
 
-    token = jwt.encode(data, SECRET_KEY, ALGORITHM)
+
+def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool= False):
+    payload = {}
+
+    payload["user"] = user_data
+    payload["exp"] = datetime.now() + (
+        expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+    )
+    payload['jti'] = str(uuid.uuid4())
+
+    payload['refresh'] = refresh
+
+
+    token = jwt.encode(
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
+    )
 
     return token
 
 
-def decode_token(token: str) -> dict | None:
-    """Get data from an access token"""
-
+def decode_token(token: str) -> dict:
     try:
-        data = jwt.decode(token, SECRET_KEY)
-        return data
+        token_data = jwt.decode(
+            jwt= token,
+            key=Config.JWT_SECRET,
+            algorithms=[Config.JWT_ALGORITHM]
+        )
 
-    except Exception:
-        return {}
-
-
-def get_user_from_jwt(token: str) -> str | None:
-    """Get user from  a given JWT"""
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-
-        if not (username := payload.get("sub")):
-            return None
-
-    except jwt.PyJWTError:
+        return token_data
+    
+    except jwt.PyJWTError as e:
+        logging.exception(e)
         return None
-
-    return username
