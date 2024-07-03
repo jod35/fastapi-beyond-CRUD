e952f405c4b18bb22d7d6aaeedc93ffbff13dc3c
jod35 <jodestrevin@gmail.com>
2024-07-03 19:38:19 +0300

    add error handlers and custom error classes

--- src/books/service.py
+++ src/books/service.py
@@ -1,117 +1,83 @@
-from fastapi import status
-from fastapi.exceptions import HTTPException
-from sqlmodel import desc, select
 from sqlmodel.ext.asyncio.session import AsyncSession
-
-from src.books.service import BookService
-from src.db.models import Tag
-
-from .schemas import TagAddModel, TagCreateModel
-from src.errors import BookNotFound, TagNotFound, TagAlreadyExists
-
-book_service = BookService()
+from .schemas import BookCreateModel, BookUpdateModel
+from sqlmodel import select, desc
+from src.db.models import Book
+from datetime import datetime
 
 
-server_error = HTTPException(
-    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong"
-)
-
-
-class TagService:
-
-    async def get_tags(self, session: AsyncSession):
-        """Get all tags"""
-
-        statement = select(Tag).order_by(desc(Tag.created_at))
+class BookService:
+    async def get_all_books(self, session: AsyncSession):
+        statement = select(Book).order_by(desc(Book.created_at))
 
         result = await session.exec(statement)
 
         return result.all()
 
-    async def add_tags_to_book(
-        self, book_uid: str, tag_data: TagAddModel, session: AsyncSession
-    ):
-        """Add tags to a book"""
-
-        book = await book_service.get_book(book_uid=book_uid, session=session)
-
-        if not book:
-            raise BookNotFound()
-
-        for tag_item in tag_data.tags:
-            result = await session.exec(select(Tag).where(Tag.name == tag_item.name))
-
-            tag = result.one_or_none()
-            if not tag:
-                tag = Tag(name=tag_item.name)
-
-            book.tags.append(tag)
-        session.add(book)
-        await session.commit()
-        await session.refresh(book)
-        return book
-
-    async def get_tag_by_uid(self, tag_uid: str, session: AsyncSession):
-        """Get tag by uid"""
-
-        statement = select(Tag).where(Tag.uid == tag_uid)
+    async def get_user_books(self, user_uid: str, session: AsyncSession):
+        statement = (
+            select(Book)
+            .where(Book.user_uid == user_uid)
+            .order_by(desc(Book.created_at))
+        )
 
         result = await session.exec(statement)
 
-        return result.first()
+        return result.all()
 
-    async def add_tag(self, tag_data: TagCreateModel, session: AsyncSession):
-        """Create a tag"""
-
-        statement = select(Tag).where(Tag.name == tag_data.name)
+    async def get_book(self, book_uid: str, session: AsyncSession):
+        statement = select(Book).where(Book.uid == book_uid)
 
         result = await session.exec(statement)
 
-        tag = result.first()
+        book = result.first()
 
-        if tag:
-            raise TagAlreadyExists()
-        new_tag = Tag(name=tag_data.name)
+        return book if book is not None else None
 
-        session.add(new_tag)
+    async def create_book(
+        self, book_data: BookCreateModel, user_uid: str, session: AsyncSession
+    ):
+        book_data_dict = book_data.model_dump()
+
+        new_book = Book(**book_data_dict)
+
+        new_book.published_date = datetime.strptime(
+            book_data_dict["published_date"], "%Y-%m-%d"
+        )
+
+        new_book.user_uid = user_uid
+
+        session.add(new_book)
 
         await session.commit()
 
-        return new_tag
+        return new_book
 
-    async def update_tag(
-        self, tag_uid, tag_update_data: TagCreateModel, session: AsyncSession
+    async def update_book(
+        self, book_uid: str, update_data: BookUpdateModel, session: AsyncSession
     ):
-        """Update a tag"""
+        book_to_update = await self.get_book(book_uid, session)
 
-        tag = await self.get_tag_by_uid(tag_uid, session)
+        if book_to_update is not None:
+            update_data_dict = update_data.model_dump()
 
-        if not tag:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-        update_data_dict = tag_update_data.model_dump()
-
-        for k, v in update_data_dict.items():
-            setattr(tag, k, v)
+            for k, v in update_data_dict.items():
+                setattr(book_to_update, k, v)
 
             await session.commit()
 
-            await session.refresh(tag)
+            return book_to_update
+        else:
+            return None
 
-        return tag
+    async def delete_book(self, book_uid: str, session: AsyncSession):
+        book_to_delete = await self.get_book(book_uid, session)
 
-    async def delete_tag(self, tag_uid: str, session: AsyncSession):
-        """Delete a tag"""
+        if book_to_delete is not None:
+            await session.delete(book_to_delete)
 
-        statement = select(Tag).where(Tag.uid == tag_uid)
+            await session.commit()
 
-        result = await session.exec(statement)
+            return {}
 
-        tag = result.first()
-
-        if not tag:
-            raise TagNotFound()
-
-        await session.delete(tag)
-
-        await session.commit()
+        else:
+            return None
