fefc30f95ae1b92dc735d6b93e2072100e597196
jod35 <jodestrevin@gmail.com>
2024-07-06 11:41:15 +0300

    fix errors

--- src/__init__.py
+++ src/__init__.py
@@ -4,20 +4,7 @@ from src.auth.routes import auth_router
 from src.books.routes import book_router
 from src.reviews.routes import review_router
 from src.tags.routes import tags_router
-from .errors import (
-    create_exception_handler,
-    InvalidCredentials,
-    TagAlreadyExists,
-    TagNotFound,
-    BookNotFound,
-    UserAlreadyExists,
-    UserNotFound,
-    InsufficientPermission,
-    AccessTokenRequired,
-    InvalidToken,
-    RefreshTokenRequired,
-    RevokedToken,
-)
+from .errors import register_error_handlers
 
 version = "v1"
 
@@ -27,128 +14,8 @@ app = FastAPI(
     version=version,
 )
 
-app.add_exception_handler(
-    UserAlreadyExists,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        initial_detail={
-            "message": "User with email already exists",
-            "error_code": "user_exists",
-        },
-    ),
-)
 
-app.add_exception_handler(
-    UserNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        initial_detail={"message": "User not found", "error_code": "user_not_found"},
-    ),
-)
-app.add_exception_handler(
-    BookNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        initial_detail={"message": "Book not found", "error_code": "book_not_found"},
-    ),
-)
-app.add_exception_handler(
-    InvalidCredentials,
-    create_exception_handler(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        initial_detail={
-            "message": "Invalid Email Or Password",
-            "error_code": "invalid_email_or_password",
-        },
-    ),
-)
-app.add_exception_handler(
-    InvalidToken,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        initial_detail={
-            "message": "Token is invalid Or expired",
-            "resolution": "Please get new token",
-            "error_code": "invalid_token",
-        },
-    ),
-)
-app.add_exception_handler(
-    RevokedToken,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        initial_detail={
-            "message": "Token is invalid or has been revoked",
-            "resolution": "Please get new token",
-            "error_code": "token_revoked",
-        },
-    ),
-)
-app.add_exception_handler(
-    AccessTokenRequired,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        initial_detail={
-            "message": "Please provide a valid access token",
-            "resolution": "Please get an access token",
-            "error_code": "access_token_required",
-        },
-    ),
-)
-app.add_exception_handler(
-    RefreshTokenRequired,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        initial_detail={
-            "message": "Please provide a valid refresh token",
-            "resolution": "Please get an refresh token",
-            "error_code": "refresh_token_required",
-        },
-    ),
-)
-app.add_exception_handler(
-    InsufficientPermission,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        initial_detail={
-            "message": "You do not have enough permissions to perform this action",
-            "error_code": "insufficient_permissions",
-        },
-    ),
-)
-app.add_exception_handler(
-    TagNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        initial_detail={"message": "Tag Not Found", "error_code": "tag_not_found"},
-    ),
-)
-
-app.add_exception_handler(
-    TagAlreadyExists,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        initial_detail={"message": "Tag Already exists", "error_code": "tag_exists"},
-    ),
-)
-
-app.add_exception_handler(
-    BookNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        initial_detail={"message": "Book Not Found", "error_code": "book_not_found"},
-    ),
-)
-
-
-@app.exception_handler(500)
-async def internal_server_error(request, exc):
-
-    return JSONResponse(
-        content={"message": "Oops! Something went wrong", "error_code": "server_error"},
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    )
-
+register_error_handlers(app)
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
--- src/errors.py
+++ src/errors.py
@@ -1,69 +1,225 @@
 from typing import Any, Callable
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from fastapi import FastAPI, status
+
 
 class BooklyException(Exception):
     """This is the base class for all bookly errors"""
+
     pass
+
 
 class InvalidToken(BooklyException):
     """User has provided an invalid or expired token"""
+
     pass
+
 
 class RevokedToken(BooklyException):
     """User has provided a token that has been revoked"""
+
     pass
 
 
 class AccessTokenRequired(BooklyException):
     """User has provided a refresh token when an access token is needed"""
+
     pass
+
 
 class RefreshTokenRequired(BooklyException):
     """User has provided an access token when a refresh token is needed"""
+
     pass
+
 
 class UserAlreadyExists(BooklyException):
     """User has provided an email for a user who exists during sign up."""
+
     pass
+
 
 class InvalidCredentials(BooklyException):
     """User has provided wrong email or password during log in."""
+
     pass
+
 
 class InsufficientPermission(BooklyException):
     """User does not have the neccessary permissions to perform an action."""
+
     pass
+
 
 class BookNotFound(BooklyException):
     """Book Not found"""
+
     pass
 
 
 class TagNotFound(BooklyException):
     """Tag Not found"""
+
     pass
+
 
 class TagAlreadyExists(BooklyException):
     """Tag already exists"""
+
     pass
-
-
 
 
 class UserNotFound(BooklyException):
     """User Not found"""
+
     pass
 
 
-def create_exception_handler(status_code: int, initial_detail:Any) -> Callable[[Request,Exception],JSONResponse]:
+def create_exception_handler(
+    status_code: int, initial_detail: Any
+) -> Callable[[Request, Exception], JSONResponse]:
 
-    async def exception_handler(request: Request,exc: BooklyException): 
+    async def exception_handler(request: Request, exc: BooklyException):
 
-        return JSONResponse(
-            content=initial_detail,
-            status_code=status_code
-        )
-    
+        return JSONResponse(content=initial_detail, status_code=status_code)
 
     return exception_handler
+
+
+def register_error_handlers(app: FastAPI):
+    app.add_exception_handler(
+        UserAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "User with email already exists",
+                "error_code": "user_exists",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        UserNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "User not found",
+                "error_code": "user_not_found",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        BookNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "Book not found",
+                "error_code": "book_not_found",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        InvalidCredentials,
+        create_exception_handler(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            initial_detail={
+                "message": "Invalid Email Or Password",
+                "error_code": "invalid_email_or_password",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        InvalidToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid Or expired",
+                "resolution": "Please get new token",
+                "error_code": "invalid_token",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        RevokedToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid or has been revoked",
+                "resolution": "Please get new token",
+                "error_code": "token_revoked",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        AccessTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Please provide a valid access token",
+                "resolution": "Please get an access token",
+                "error_code": "access_token_required",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        RefreshTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "Please provide a valid refresh token",
+                "resolution": "Please get an refresh token",
+                "error_code": "refresh_token_required",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        InsufficientPermission,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "You do not have enough permissions to perform this action",
+                "error_code": "insufficient_permissions",
+            },
+        ),
+    )
+    app.add_exception_handler(
+        TagNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={"message": "Tag Not Found", "error_code": "tag_not_found"},
+        ),
+    )
+
+    app.add_exception_handler(
+        TagAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Tag Already exists",
+                "error_code": "tag_exists",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        BookNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "Book Not Found",
+                "error_code": "book_not_found",
+            },
+        ),
+    )
+
+    @app.exception_handler(500)
+    async def internal_server_error(request, exc):
+
+        return JSONResponse(
+            content={
+                "message": "Oops! Something went wrong",
+                "error_code": "server_error",
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
