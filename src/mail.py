38848e7b80bc92fad8ab282dc20746a045338338
jod35 <jodestrevin@gmail.com>
2024-08-01 14:07:34 +0300

    add missing dependency passlib

--- src/mail.py
+++ src/mail.py
@@ -16,7 +16,7 @@ mail_config = ConnectionConfig(
     MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True,
-    TEMPLATE_FOLDER=Path(BASE_DIR, "templates"),
+    # TEMPLATE_FOLDER=Path(BASE_DIR, "templates"),
 )
 
 
