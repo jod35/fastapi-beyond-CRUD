18e38a47052a2ed304eac7b395ac4d00257fcdc1
jod35 <jodestrevin@gmail.com>
2024-08-05 13:37:11 +0300

    add prod URL in allowed hosts

--- src/middleware.py
+++ src/middleware.py
@@ -32,4 +32,4 @@ def register_middleware(app: FastAPI):
         allow_credentials=True,
     )
 
-    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1"])
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1","fastapi-beyond-crud.onrender.com"])
